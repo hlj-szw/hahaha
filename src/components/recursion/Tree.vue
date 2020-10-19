b1d4c72195ee85a1f599a8e49eca353587b50e38
hlj <hlj@allfree.cc>
2020-10-19 15:02:44 +0800

    h

---
+++ src/components/recursion/Tree.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="kkb-tree">
+    <TreeNode v-for="item in data" :key="item.title" :model="item"></TreeNode>
+  </div>
+</template>
+
+<script>
+import TreeNode from "@/components/recursion/TreeNode.vue";
+export default {
+  props: {
+    data: {
+      type: Array,
+      required:true
+    },
+  },
+  components: {
+    TreeNode,
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/recursion/TreeNode.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+      <div class="treenode" @click="toggle">
+        <label>{{model.title}}</label>
+        <span v-if="isFolder">[{{open?"-":"+"}}]</span>
+      </div>
+      <div v-show="open" v-if="isFolder">
+        <tree-node v-for="item in model.children" :key="item.title" :model="item"></tree-node>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'tree-node',
+  props: {
+    model: {
+      type: Object,
+    },
+  },
+  data() {
+    return {
+      open:false
+    }
+  },
+  methods: {
+    toggle() {
+      if(this.isFolder){
+        this.open=!this.open;
+      }
+    }
+  },
+  computed: {
+    isFolder() {
+      return this.model.children&&this.model.children.length>0
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
